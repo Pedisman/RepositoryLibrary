e64a62d62a8167aac7f8bca0faaf192eb6b09cde
Pedisman <timchumpo@gmail.com>
2018-08-30 00:07:50 +1200

    Added _readerOutputSourceFactory, _commandSourceFactory, _dataReaderGetter and _dbCommandExecutor interfaces. Added filter list to AddParameter methods and executeHelpers to include only parameters specified in an input list.

--- RepositoryLibrary/Database/RepositoryBase.cs
+++ RepositoryLibrary/Database/RepositoryBase.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using RepositoryLibrary.Extensions;
 using System.Reflection;
 using RepositoryLibrary.Attributes;
-using RepositoryLibrary.OutputSources;
 using System.Data.SqlClient;
 using System.Collections.ObjectModel;
 using System.Data;
 using RepositoryLibrary.Interfaces;
+using System.Linq;
+using RepositoryLibrary.Factories;
+using RepositoryLibrary.DataReaderGetters;
+using RepositoryLibrary.IDbCommandExecutors;
 
 namespace RepositoryLibrary.Database
 {
     public abstract class RepositoryBase
     {
         private string _connectionString;
+        private IOutputSourceFactory _readerOutputSourceFactory;
+        private IOutputSourceFactory _commandOutputSourceFactory;
+        private IDataReaderGetter _dataReaderGetter;
+        private IDbCommandExecutor _dbCommandExecutor;
+
         public RepositoryBase(string connectionString)
         {
             _connectionString = connectionString;
+            _readerOutputSourceFactory = new ReaderOutputSourceFactory();
+            _commandOutputSourceFactory = new CommandOutputSourceFactory();
+            _dataReaderGetter = new SqlDataReaderGetter();
+            _dbCommandExecutor = new SqlCommandExecutor();
+        }
+
+        public RepositoryBase(string connectionString, 
+            IOutputSourceFactory readerOutputSourceFactory,
+            IOutputSourceFactory commandOutputSourceFactory,
+            IDataReaderGetter dataReaderGetter,
+            IDbCommandExecutor dbCommandExecutor)
+        {
+            _connectionString = connectionString;
+            _readerOutputSourceFactory = readerOutputSourceFactory;
+            _commandOutputSourceFactory = commandOutputSourceFactory;
+            _dataReaderGetter = dataReaderGetter;
+            _dbCommandExecutor = dbCommandExecutor;
         }
 
         #region Connection Helpers
@@ -48,6 +72,25 @@ namespace RepositoryLibrary.Database
         #endregion
 
         #region ExecuteHelpers
+        public static Dictionary<string, PropertyInfo> GetDatabaseAttributes<A>(Type instanceType, IEnumerable<string> filter)            
+            where A : DAL_AttributeBase
+        {         
+            Type attrType = typeof(A);
+            var properties = instanceType.GetProperties();
+            Dictionary<string, PropertyInfo> attrDictionary = new Dictionary<string, PropertyInfo>();
+            foreach (var prop in properties)
+            {
+                if (!Attribute.IsDefined(prop, attrType)) continue;
+                var attr = prop.GetCustomAttribute(attrType) as A;
+                if (filter != null && !filter.Contains(attr.Name)) continue;
+                string attrName = attr.Name;
+                if (string.IsNullOrWhiteSpace(attrName))
+                    throw new Exception($"ColumAttribute name not specified: {instanceType.AssemblyQualifiedName}");
+                attrDictionary.Add(attrName, prop);
+            }
+            return attrDictionary;
+        }
+
         private T ExecuteHelper<T>(IOutputSource src, Dictionary<string, PropertyInfo> attrDictionary) where T : class
         {
             var obj = Activator.CreateInstance<T>();
@@ -68,29 +111,29 @@ namespace RepositoryLibrary.Database
             }
         }
 
-        private T ExecuteNonQueryHelper<T>(SqlCommand command, string commandText, Action<SqlConnection, SqlCommand, string> commandSetup)
+        private T ExecuteNonQueryHelper<T>(SqlCommand command, string commandText, Action<SqlConnection, SqlCommand, string> commandSetup, IEnumerable<string> filter)
             where T : class
         {
             using (SqlConnection connection = GetConnection())
             {
                 commandSetup(connection, command, commandText);
-                command.ExecuteNonQuery();
-                var attrDictionary = Activator.CreateInstance<T>().GetDatabaseAttributes<T, ParameterAttribute>();
-                return ExecuteHelper<T>(new CommandOutputSource(command), attrDictionary);
+                command = _dbCommandExecutor.ExecuteNonQuery(command) as SqlCommand;
+                var attrDictionary = GetDatabaseAttributes<ParameterAttribute>(typeof(T), filter);
+                return ExecuteHelper<T>(_commandOutputSourceFactory.Create(command), attrDictionary);
             }
         }
-        private IEnumerable<T> ExecuteReaderHelper<T>(SqlCommand command, string commandText, Action<SqlConnection, SqlCommand, string> commandSetup) where T : class
+        private IEnumerable<T> ExecuteReaderHelper<T>(SqlCommand command, string commandText, Action<SqlConnection, SqlCommand, string> commandSetup, IEnumerable<string> filter) where T : class
         {
             using (SqlConnection connection = GetConnection())
             {
                 commandSetup(connection, command, commandText);
-                using (SqlDataReader reader = command.ExecuteReader())
+                using (IDataReader reader = _dataReaderGetter.Get(command))
                 {
                     Collection<T> result = new Collection<T>();
                     while (reader.Read())
                     {
-                        var attrDictionary = Activator.CreateInstance<T>().GetDatabaseAttributes<T, ColumnAttribute>();
-                        result.Add(ExecuteHelper<T>(new ReaderOutputSource(reader), attrDictionary));
+                        var attrDictionary = GetDatabaseAttributes<ColumnAttribute>(typeof(T), filter);
+                        result.Add(ExecuteHelper<T>(_readerOutputSourceFactory.Create(reader), attrDictionary));
                     }
                     return result;
                 }
@@ -107,8 +150,8 @@ namespace RepositoryLibrary.Database
             param.Value = value;
             return param;
         }
-        private void AddParametersHelper<T>(SqlCommand command, T instance, ParameterDirection direction) where T : class
-        {
+        private void AddParametersHelper<T>(SqlCommand command, T instance, ParameterDirection direction, IEnumerable<string> filter) where T : class
+        {            
             Type instanceType = instance.GetType();
             Type attrType = typeof(ParameterAttribute);
             var properties = instanceType.GetProperties();
@@ -116,20 +159,21 @@ namespace RepositoryLibrary.Database
             {
                 if (!Attribute.IsDefined(prop, attrType)) continue;
                 var attr = prop.GetCustomAttribute(attrType) as ParameterAttribute;
-                command.Parameters.Add(CreateParameter(command, prop.GetValue(instance), attr, direction));
+                if (filter == null || filter.Contains(attr.Name))                
+                    command.Parameters.Add(CreateParameter(command, prop.GetValue(instance), attr, direction));
             }
         }
         #endregion
 
         #region Client Methods
-        protected void AddInputParameters<T>(SqlCommand command, T instance) where T : class
+        protected void AddInputParameters<T>(SqlCommand command, T instance, IEnumerable<string> filter = null) where T : class
         {
-            AddParametersHelper(command, instance, ParameterDirection.Input);
+            AddParametersHelper(command, instance, ParameterDirection.Input, filter);
         }
 
-        protected void AddOutputParameters<T>(SqlCommand command, T instance) where T : class
+        protected void AddOutputParameters<T>(SqlCommand command, T instance, IEnumerable<string> filter = null) where T : class
         {
-            AddParametersHelper(command, instance, ParameterDirection.Output);
+            AddParametersHelper(command, instance, ParameterDirection.Output, filter);
         }
 
         protected void ExecuteNonQuery(SqlCommand command, string commandText)
@@ -142,24 +186,24 @@ namespace RepositoryLibrary.Database
             ExecuteNonQueryHelper(command, commandText, SetupStoredProcedure);
         }
 
-        protected T ExecuteNonQuery<T>(SqlCommand command, string commandText) where T : class
+        protected T ExecuteNonQuery<T>(SqlCommand command, string commandText, IEnumerable<string> filter = null) where T : class
         {
-            return ExecuteNonQueryHelper<T>(command, commandText, SetupTextCommand);
+            return ExecuteNonQueryHelper<T>(command, commandText, SetupTextCommand, filter);
         }
 
-        protected T ExecuteNonQuerySP<T>(SqlCommand command, string commandText) where T : class
+        protected T ExecuteNonQuerySP<T>(SqlCommand command, string commandText, IEnumerable<string> filter = null) where T : class
         {
-            return ExecuteNonQueryHelper<T>(command, commandText, SetupStoredProcedure);
+            return ExecuteNonQueryHelper<T>(command, commandText, SetupStoredProcedure, filter);
         }
 
-        protected IEnumerable<T> ExecuteReader<T>(SqlCommand command, string commandText) where T : class
+        protected IEnumerable<T> ExecuteReader<T>(SqlCommand command, string commandText, IEnumerable<string> filter = null) where T : class
         {
-            return ExecuteReaderHelper<T>(command, commandText, SetupTextCommand);
+            return ExecuteReaderHelper<T>(command, commandText, SetupTextCommand, filter);
         }
 
-        protected IEnumerable<T> ExecuteReaderSP<T>(SqlCommand command, string commandText) where T : class
+        protected IEnumerable<T> ExecuteReaderSP<T>(SqlCommand command, string commandText, IEnumerable<string> filter = null) where T : class
         {
-            return ExecuteReaderHelper<T>(command, commandText, SetupStoredProcedure);
+            return ExecuteReaderHelper<T>(command, commandText, SetupStoredProcedure, filter);
         }
         #endregion
     }
