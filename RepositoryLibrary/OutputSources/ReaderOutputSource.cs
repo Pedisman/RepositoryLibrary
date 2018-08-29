9e01d643fb0d807431cbd0a91dda7fc97379cd63
Pedisman <timchumpo@gmail.com>
2018-08-30 00:03:01 +1200

    Changed input parameter to object type and used internal casting.

--- RepositoryLibrary/OutputSources/ReaderOutputSource.cs
+++ RepositoryLibrary/OutputSources/ReaderOutputSource.cs
@@ -7,8 +7,10 @@ namespace RepositoryLibrary.OutputSources
     public class ReaderOutputSource : IOutputSource
     {
         private SqlDataReader _reader;
-        public ReaderOutputSource(SqlDataReader reader)
+        public ReaderOutputSource(object sqlReader)
         {
+            SqlDataReader reader = sqlReader as SqlDataReader;
+            if (reader == null) throw new ArgumentException("sqlReader");
             _reader = reader;
         }
         public object this[string key]
