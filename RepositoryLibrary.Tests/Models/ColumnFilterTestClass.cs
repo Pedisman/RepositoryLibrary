5bb6804fdc08c568a487f41b7c9269ba782ea01c
Pedisman <timchumpo@gmail.com>
2018-09-08 19:36:49 +1200

    Added AttributeTestClass to facilitate AttributeExtension method tests.

---
+++ RepositoryLibrary.Tests/Models/ColumnFilterTestClass.cs
@@ -0,0 +1,19 @@
+﻿using RepositoryLibrary.Attributes;
+
+namespace RepositoryLibrary.Tests.Models
+{
+    public class AttributeTestClass
+    {
+        [Column(Name = "FIRST_NAME")]
+        [Parameter(Name = "@firstName", Type = System.Data.SqlDbType.NVarChar, Length = 50)]
+        public string FirstName { get; set; }
+
+        [Column(Name = "LAST_NAME")]
+        [Parameter(Name = "@lastName", Type = System.Data.SqlDbType.NVarChar, Length = 50)]
+        public string LastName { get; set; }
+
+        [Column(Name = "AGE")]
+        [Parameter(Name = "@age", Type = System.Data.SqlDbType.Int)]
+        public int Age { get; set; }
+    }
+}
