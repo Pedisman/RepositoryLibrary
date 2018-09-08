5beb8cfad2ba62e8c4bde8f220069c32fb5bf235
Pedisman <timchumpo@gmail.com>
2018-09-08 19:37:15 +1200

    Added AttributeExtension unit tests module.

---
+++ RepositoryLibrary.Tests/AttributeExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using RepositoryLibrary.Attributes;
+using RepositoryLibrary.Tests.Models;
+
+namespace RepositoryLibrary.Tests
+{
+    [TestFixture]
+    public class AttributeExtensionsTests
+    {
+        [TestCase]
+        public void GetColumnName_returnsColumnName()
+        {
+            AttributeTestClass testClass = new AttributeTestClass();
+            string result = testClass.GetColumnName(x => x.Age);
+            Assert.AreEqual("AGE", result);
+        }
+
+        [TestCase]
+        public void GetParameterName_returnsParameterName()
+        {
+            AttributeTestClass testClass = new AttributeTestClass();
+            string result = testClass.GetParameterName(x => x.Age);            
+            Assert.AreEqual("@age", result);
+        }
+    }
+}
