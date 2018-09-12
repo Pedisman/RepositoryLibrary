ca3b7ad7b035605bf4dcd02e6d71377a721b8008
Pedisman <timchumpo@gmail.com>
2018-09-12 22:49:40 +1200

    Added test class for checking nullable value types are handled correctly.

---
+++ RepositoryLibrary.Tests/Models/ClassWithNullableTypes.cs
@@ -0,0 +1,13 @@
+﻿using RepositoryLibrary.Attributes;
+
+namespace RepositoryLibrary.Tests.Models
+{
+    public class ClassWithNullableTypes
+    {
+        [Column(Name = "QUANTITY")]
+        public int Quantity { get; set; }
+
+        [Column(Name = "PRICE")]
+        public double? Price { get; set; }
+    }
+}
